aa9da175e2c1b91cc357b41220f32cbf8fc219a8
luul negasi <luultesfay24@gmail.com>
2021-09-09 09:43:49 -0700

    sumbmit the form,clear input field,interchange visblity of field with toggle

--- script.js
+++ script.js
@@ -8,10 +8,12 @@ const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
 const form = document.querySelector(".form");
 const containerWorkouts = document.querySelector(".workouts");
 const inputType = document.querySelector(".form__input--type");
-const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
+const inputDistance = document.querySelector(".form__input--distance");
 const inputElevation = document.querySelector(".form__input--elevation");
+
+let map, mapEvent;
 
 //<--!we added CDN link of liflet library and add differ manually--->  in the html file that the code we got from library
 ///////////229. Using the Geolocation API
@@ -33,7 +35,7 @@ if (navigator.geolocation)
       //Leaflet Library is an open-source JavaScript libraryfor mobile-friendly interactive maps
       // and in this section we will include the  Using a Hosted Version of Leaflet   we will copy from the website and will put in the head of our html file
 
-      const map = L.map("map").setView(coords, 13); //we change var with const const  and     [51.505, -0.09] with coords   and also 13 is like zoomin and zoom out we will change it and we will inspect on the map
+      map = L.map("map").setView(coords, 13); //we change var with const const  and     [51.505, -0.09] with coords   and also 13 is like zoomin and zoom out we will change it and we will inspect on the map
 
       L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
         attribution:
@@ -48,28 +50,53 @@ if (navigator.geolocation)
       ////////////231. Displaying a Map Marker
       //So in this video we're gonna display a marker  wherever we click on the map.
 
-      map.on("click", function (mapEvent) {
-        //by using this we desplay all markers that we click
-        //console.log(mapEvent); //we will get the cliked event in that particular location
-        const { lat, lng } = mapEvent.latlng; //we destracturing      lat and lang  stands for latitude and longutude
-        //L.marker([lat, lng]).addTo(map).bindPopup("working out").openPopup(); //working out is displays with the marker
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              //get this from liflet
-              maxWidth: 200,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: "running-popup",
-            })
-          )
-          .setPopupContent("working out") //this method is from lief let
-          .openPopup();
+      //we rendered(displayed) the form  when we click the map
+      map.on("click", function (mapE) {
+        mapEvent = mapE;
+        form.classList.remove("hidden"); //when we click on the map the form is rendered or opened in the left side  becouse it was hidden in the html and we remove it
+        inputDistance.focus();
       });
     },
     function () {
-      alert("i coudnt get the location 😕");
+      alert("i coudn't get the location 😕");
     }
   );
+
+//then we will sumbmit the form
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  //clear input field
+
+  inputCadence.value =
+    inputDistance.value =
+    inputDuration.value =
+    inputElevation.value =
+      "";
+
+  //by using this we display all markers that we click
+
+  //console.log(mapEvent); //we will get the cliked event in that particular location
+  const { lat, lng } = mapEvent.latlng; //we destracturing      lat and lang  stands for latitude and longutude
+  //L.marker([lat, lng]).addTo(map).bindPopup("working out").openPopup(); //working out is displays with the marker
+  L.marker([lat, lng])
+    .addTo(map) //map and mapEvent   are out of scope  we need to make the  global variable  to access every where
+    .bindPopup(
+      L.popup({
+        //get this from liflet
+        maxWidth: 200,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: "running-popup",
+      })
+    )
+    .setPopupContent("working out") //this method is from lief let
+    .openPopup();
+});
+//this willchange element in the in the input  when one is displayed the other one hidden  in tis case the  cycling(cadance) and runing (elevetion gain)interchanching
+inputType.addEventListener("change", function () {
+  inputCadence.closest(".form__row").classList.toggle("form__row--hidden");
+  inputElevation.closest(".form__row").classList.toggle("form__row--hidden");
+});
